f5165020b0c8c51019183d3d5bc413d043e3be18
Lakshmikanta Patra <frontendfixer@gmail.com>
2023-07-25 17:11:01 +0530

    add cart/product page and their corresponding provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
+import Cart from './components/Cart'
+import ProductList from './components/ProductList'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
-  return (
+  const [viewCart, setViewCart] = useState(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
     <>
-      <div className="App">
-        <h1>Initial Title</h1>
-      </div>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
     </>
   )
+
+  return content
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { CartProvider } from './context/CartProvider.tsx'
+import { ProductProvider } from './context/ProductsProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ProductProvider>
   </React.StrictMode>
 )
